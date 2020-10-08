f919740bb700268a74a50137d95ae3e758ee3cfd
Andreizayaz <poznavauka@mail.ru>
2020-10-08 20:22:12 +0300

    feat: solve task 'reverse int'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str;
+    let arr;
+
+    str = (n < 0) ? Math.abs(n).toString() : n.toString();
+
+    arr = str.split('').reverse();
+
+    str = (arr[0]===0)?arr.slice(1).join(''):arr.join('');
+
+    return str;
 }
